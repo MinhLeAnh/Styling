42408fbc53aad5b70f7da35d82bd424b5b743432
MinhLeAnh <leminhst24@gmail.com>
2024-07-15 13:33:29 +0700

    first committed
    first committed

--- App.js
+++ App.js
@@ -1,11 +1,18 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View,Text } from 'react-native';
+
+const Square = ({ backgroundColor,text }) => {
+  return <View style={[styles.square, { backgroundColor }]} >
+    <Text>{text}</Text>
+  </View>;
+};
+
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Square backgroundColor="red" text="square 1" />
+      <Square backgroundColor="green" text="square 2" />
+      <Square backgroundColor="#cde" text="square 3"/>
     </View>
   );
 }
@@ -14,6 +21,14 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+    flexDirection:'row'
+  },
+  square: {
+    width: 100,
+    height: 100,
+    margin: 10,
     alignItems: 'center',
     justifyContent: 'center',
   },
